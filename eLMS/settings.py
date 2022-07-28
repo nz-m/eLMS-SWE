4f1eb35d02a1eb138bc7cd789892b13ca398f7fa
Neaz Mahmud <neaz6160@gmail.com>
2022-07-29 01:15:44 +0600

    Update settings.py

--- eLMS/settings.py
+++ eLMS/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
-    'discussion.apps.DiscussionConfig',
-
+  
 ]
 
 MIDDLEWARE = [
