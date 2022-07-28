286f2d6a2340b359388c2b774df7ccc6069bfcec
Neaz Mahmud <neaz6160@gmail.com>
2022-07-29 01:26:19 +0600

    change db settings

--- eLMS/settings.py
+++ eLMS/settings.py
@@ -79,22 +79,22 @@ WSGI_APPLICATION = 'eLMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'elms',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': '',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'elms',
+#         'HOST': 'localhost',
+#         'USER': 'root',
+#         'PASSWORD': '',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
