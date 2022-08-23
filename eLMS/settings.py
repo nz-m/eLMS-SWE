612348799d7c5bfc3c4ab008565f37b864bc4501
Neaz Mahmud <neaz6160@gmail.com>
2022-08-23 23:32:49 +0600

    add department model

--- eLMS/settings.py
+++ eLMS/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'eLMS.wsgi.application'
 #         'NAME': 'elms',
 #         'HOST': 'localhost',
 #         'USER': 'root',
-#         'PASSWORD': '',
+#         'PASSWORD': '4321',
 #     }
 # }
 
--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Student, Faculty, Course 
+from .models import Student, Faculty, Course , Department
 
 admin.site.register(Student)
 admin.site.register(Faculty)
 admin.site.register(Course)
+admin.site.register(Department)
